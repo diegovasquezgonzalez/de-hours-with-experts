6f1e94c850e34e7fc4f5c1ebf5566e21944d2934
Diego Enrique Vasquez Gonzalez <diegovasquez@Diegos-MBP.lan>
2024-09-08 09:33:14 -0500

    Secret recipe decoder ready

--- python/secret_recipe_decoder.py
+++ python/secret_recipe_decoder.py
@@ -51,21 +51,45 @@ class Ingredient():
         self.description = description
 
 
-def decode_string(str):
-    """Given a string named str, use the Caesar encoding above to return the decoded string."""
-    # TODO: implement me
-    return '1 cup'
-
+def decode_string(input_string):
+    """Given a string named input_string, use the Caesar encoding above to return the decoded string."""
+    # TODO: implement me 
+    decoded_list = []
+    for char in input_string:
+        if char == '#':
+            decoded_list.append(' ')
+        elif char in ENCODING:
+            decoded_list.append(ENCODING[char])
+        else:
+            decoded_list.append(char)
+    return ''.join(decoded_list)
 
 def decode_ingredient(line):
     """Given an ingredient, decode the amount and description, and return a new Ingredient"""
     # TODO: implement me
-    return Ingredient("1 cup", "butter")
 
+    parts = line.split('#', 1)
+    if len(parts) == 2:
+        qty_encoded, name_encoded = parts
+    else:
+        qty_encoded = parts[0]
+        name_encoded = ''
+    
+    qty_decoded = decode_string(qty_encoded)
+    name_decoded = decode_string(name_encoded)
+
+    return Ingredient(qty_decoded, name_decoded)
 
 def main():
     """A program that decodes a secret recipe"""
     # TODO: implement me
+
+    with open('secret_recipe.txt', 'r') as file:
+        encoded_recipe = file.readlines()
+
+    with open('decoded_recipe.txt', 'w') as output_file:
+        for line in encoded_recipe:
+            output_file.write(decode_string(line) + '\n')
 
 if __name__ == "__main__":
     main()
